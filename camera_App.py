292afd09580c225b1627fb9feb805a0c77ec2a01
shawn.cooper <shawn.cooper@digitalcloak.net>
2024-04-26 09:51:03 +0200

    TroubleShooting accessing x,y points

--- camera_App.py
+++ camera_App.py
@@ -45,7 +45,7 @@ def process_frames():
         while True:
             success, image = camera.read()
             if not success:
-                printf("Ignoring empty camera frame.")
+                print("Ignoring empty camera frame.")
                 continue
             
             #Convert image to RGB format
@@ -62,6 +62,28 @@ def process_frames():
                         mp_drawing_styles.get_default_hand_landmarks_style(),
                         mp_drawing_styles.get_default_hand_connections_style()
                     )
+                
+                for hand_landmarks in results.multi_hand_landmarks.landmark:
+                    x_max = 0
+                    y_max = 0
+                    x_min = 0
+                    y_min = 0
+                    print(landmarks)
+                    """
+                    EHH not really working yet
+                    for lm in handLMs.landmark:
+                        x, y = int(lm.x * w), int(lm.y * h)
+                        if x > x_max:
+                            x_max = x
+                        if x < x_min:
+                            x_min = x
+                        if y > y_max:
+                            y_max = y
+                        if y < y_min:
+                            y_min = y
+                    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+                    mp_drawing.draw_landmarks(frame, handLMs, mphands.HAND_CONNECTIONS)
+                    """
                     
             if capture:
                 capture = 0
